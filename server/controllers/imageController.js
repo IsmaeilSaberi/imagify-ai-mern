b755144edbaaeadd45c8d467edf180fb35e5efd9
Ismaeil <ismaeilsaberi@gmail.com>
2024-12-02 21:03:29 -0800

    completing generate image api functionality but some errors occured

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,6 +1,8 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
+import FormData from "formdata";
+import axios from "axios";
 
-export const generateImage = async (req, res) => {
+const generateImage = async (req, res) => {
   try {
     const { userId, prompt } = req.body;
 
@@ -17,8 +19,38 @@ export const generateImage = async (req, res) => {
         creditBalance: user.creditBalance,
       });
     }
+
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: {
+          "x-api-key": process.env.CLIPDROP_API,
+        },
+        responseType: "arraybuffer",
+      }
+    );
+
+    const base64Image = Buffer.from(data, "binary").toString("base64");
+    const resultImage = `data:image/png;base64,${base64Image}`;
+
+    await userModel.findByIdAndUpdate(user._id, {
+      creditBalance: user.creditBalance - 1,
+    });
+
+    res.json({
+      success: true,
+      message: "Image generated",
+      creditBalance: user.creditBalance - 1,
+      resultImage,
+    });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 };
+
+export { generateImage };
